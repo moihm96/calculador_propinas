21e1e30bbbe6954d3de1811287a7b8043ac1146e
Moises Heredia Molina <moises.molina@karnovgroup.com>
2024-04-26 11:42:10 +0100

    first commit

---
+++ src/components/MenuItem.tsx
@@ -0,0 +1,17 @@
+import type { MenuItem } from "../data/types";
+
+type MenuItemProps = {
+  item: MenuItem;
+  addItem: (item: MenuItem) => void;
+};
+export default function MenuItem({ item, addItem }: MenuItemProps) {
+  return (
+    <button
+      className="border-2 hover:bg-teal-200 border-teal-400 w-full p-3 flex justify-between"
+      onClick={() => addItem(item)}
+    >
+      <p>{item.name}</p>
+      <p className="font-black">${item.price}</p>
+    </button>
+  );
+}
